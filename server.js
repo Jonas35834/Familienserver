0efe32377773216f351b520e3efbdfa38a3e44d4
Jonas35834 <jonas.hohl@ohgnagold.onmicrosoft.com>
2025-09-02 14:51:10 +0200

    server.js

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+// Benutzerliste (Speicherung nur im Speicher!)
 let users = [{ username: "admin", password: "admin" }];
 let messages = [];
 
-app.use(express.static(__dirname)); // index.html ausliefern
+// Statische Dateien (index.html ausliefern)
+app.use(express.static(path.join(__dirname)));
 
 io.on("connection", (socket) => {
+  console.log("🔌 Neuer Client verbunden");
+
+  // Login prüfen
   socket.on("login", (data) => {
     const user = users.find(
       (u) => u.username === data.username && u.password === data.password
@@ -20,9 +26,19 @@ io.on("connection", (socket) => {
     }
   });
 
+  // Chat-Nachricht empfangen
   socket.on("chat", (msg) => {
     messages.push(msg);
     io.emit("chat", msg);
+  });
+
+  // Admin erstellt neuen Benutzer
+  socket.on("add_user", (data) => {
+    if (data.username && data.password) {
+      users.push({ username: data.username, password: data.password });
+      console.log("✅ Neuer Benutzer hinzugefügt:", data.username);
+      io.emit("chat", { user: "System", text: "Benutzer " + data.username + " wurde erstellt." });
+    }
   });
 });
 
